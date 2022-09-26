68bd81976e7f045122374094de65d71a6c8d028a
perekrist <k.peregudova@redmadrobot.com>
2022-09-26 11:49:59 +0700

    Initial Commit

---
+++ Breathing/BreathingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingApp.swift
+//  Breathing
+//
+//  Created by Кристина Перегудова on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BreathingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breathing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breathing
+//
+//  Created by Кристина Перегудова on 26.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
